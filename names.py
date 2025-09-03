15ccc151c2ce2bcf89ceb36230eb0651ef8191d6
Ronnie Ng <ronnieng1514@gmail.com>
2025-09-03 13:13:16 -0400

    added name

--- names.py
+++ names.py
@@ -27,12 +27,15 @@ def anis():
 def gregory():
     letters = [71, 114, 101, 103, 111, 114, 121]
     return ''.join(chr(letter) for letter in letters)
+def ronnie(): 
+    l = [114,111,110,110,105,101] 
+    return ''.join(chr(i) for i in l)
 
 #------------------------------------------------------------------------------
 
 
 # Add your function to the list here
-NAME_FUNCTIONS = [james, mario, hasib, jian, anis, gregory]
+NAME_FUNCTIONS = [james, mario, hasib, jian, anis, gregory,ronnie]
 
 
 # Don't edit this
